0fdd20a87781db3c40ad9be4031c2e8f85a6899e
sekorme <asiedua4@uni.coventry.ac.uk>
2025-03-01 16:21:08 +0000

    new pwa set

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,81 @@
-/** @type {import('next').NextConfig} */
-import withPWA from "next-pwa"
-const nextConfig = {
-    distDir: "build",
-    reactStrictMode: true,
-    swcMinify:true,
-    compiler:{
-        removeConsole: process.env.NODE_ENV !=="development"
-    },
+import withPWA from 'next-pwa';
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
     async rewrites() {
         return [
             { source: "/robots.txt", destination: "/api/robots" },
         ];
     },
-    images:{
-        domains: ['images.unsplash.com',"pixabay.com","cloud.appwrite.io","avatar.vercel.sh"],
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'pixabay.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cloud.appwrite.io',
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatar.vercel.sh',
+            }
+        ],
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'strict-origin-when-cross-origin',
+                    },
+                ],
+            },
+            {
+                source: '/sw.js',
+                headers: [
+                    {
+                        key: 'Content-Type',
+                        value: 'text/javascript', // Add a value here
+                    },
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-cache, no-store, must-revalidate',
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src 'self'; script-src 'self'",
+                    },
+                ],
+            },
+        ]
     },
 };
 
-export default withPWA({dest:"public", register:true, skipWaiting:true, disable: process.env.NODE_ENV === "development"})(nextConfig);
+export default withPWA({
+
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+
+})({nextConfig});
